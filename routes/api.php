1913884fe5ff0b15d227af71f21f5ef5664d13b7
nodelik <161203867+Kolik28@users.noreply.github.com>
2025-11-04 21:51:07 +0800

    y

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+// Forward Vercel requests to Laravel's standard index.php entry-point.
+require __DIR__ . '/../public/index.php';
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
